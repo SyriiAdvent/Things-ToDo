33e87b721f1b9fe516423eee49920a2233898062
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-06-29 17:37:22 -0700

    basic to-do with user input and list populating

--- App.tsx
+++ App.tsx
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+interface IToDo {
+  id: string
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+  const [thingsToDo, setThingsToDo] = useState<IToDo[]>([]);
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const handleChange = (enteredText: string) => {
+    setInputValue(enteredText)
+  }
+
+  const addToDo = (item: IToDo) => {
+    setThingsToDo([...thingsToDo, item]);
+    setInputValue('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.startText}>Things</Text>
+
+      <View style={styles.mainHeader}>
+        <TextInput
+          placeholder=' Things to do.'
+          style={styles.toDoInputContainer}
+          onChangeText={handleChange}
+          value={inputValue}
+        />
+        <Button title='Add' onPress={() => addToDo({ id: Math.random().toString(), text: inputValue, completed: false })} />
+      </View>
+
+      <View>
+        {thingsToDo.map(item => (
+          <View key={item.id} style={styles.toDoListItem} >
+            <Text style={{ flex: 1, paddingLeft: 10 }}>{item.text}</Text>
+            <Button title='DELETE' onPress={() => null} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    borderColor: "red",
+    borderWidth: 1,
   },
+
+  mainHeader: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
+  toDoInputContainer: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+
+  startText: {
+    fontSize: 25,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+
+  toDoListItem: {
+    flexDirection: 'row',
+    justifyContent: "space-evenly",
+    alignItems: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginTop: 20,
+    paddingBottom: 5
+  }
 });
